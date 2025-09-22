491b111865d7a85666326e5ea0e2c40ad6e078b9
DevWachira01 <iwachiradenny@gmail.com>
2025-09-22 19:03:55 +0300

    Adding website files and styling

---
+++ script/main.js
@@ -0,0 +1,9 @@
+//Mobile Navbar Toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navbar = document.getElementById("navbar");
+
+  menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("show");
+  });
+});
